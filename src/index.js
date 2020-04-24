77ebdb82ffbd35ed34213d6f7568e15f3532c5bf
pablomdd <pablo.mdd.007@gmail.com>
2020-04-24 18:44:59 -0500

    Add webpack config

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import HelloWorld from './HelloWorld'
+import HelloWorld from './components/HelloWorld';
 
 ReactDOM.render( <HelloWorld/>, document.getElementById(app) );
