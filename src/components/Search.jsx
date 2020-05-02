f8f5eb1fab3710be433168f60129e7b7675fcb04
pablomdd <pablo.mdd.007@gmail.com>
2020-05-01 19:47:31 -0500

    Add search bar

---
+++ src/components/Search.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import '../assets/styles/components/Search.scss'
+
+const Search = () => (
+  <section class="main">
+    <h2 class="main__title">¿Qué quieres ver hoy?</h2>
+    <input type="text" class="input" placeholder="Buscar..." />
+  </section>
+);
+
+export default Search;
