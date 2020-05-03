984fd46cdbd73cf0c7cb3c2bf39a797d74696ddb
pablomdd <pablo.mdd.007@gmail.com>
2020-05-02 19:54:33 -0500

    Rename src/App 2 Home, add router in Routes

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -11,7 +11,7 @@ import "../assets/styles/App.scss";
 
 const API = "http://localhost:3000/initialState";
 
-const App = () => {
+const Home = () => {
     const initialState = useInitialState(API);
 
   // console.log(videos);
@@ -57,4 +57,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default Home;
