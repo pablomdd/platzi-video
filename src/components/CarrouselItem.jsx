e65fed3d517b52a0c9320f772dcae5f7d92d2300
pablomdd <pablo.mdd.007@gmail.com>
2020-05-19 20:02:19 -0500

    Add Player container

--- src/components/CarrouselItem.jsx
+++ src/components/CarrouselItem.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { setFavorite, deleteFavorite } from "../actions";
 
@@ -27,11 +28,13 @@ const CarrouselItem = (props) => {
       <img className="carousel-item__img" src={cover} alt={title} />
       <div className="carousel-item__details">
         <div>
-          <img
-            className="carousel-item__details--img"
-            src={playIcon}
-            alt="Play Icon"
-          />
+          <Link to={`player/${id}`}>
+            <img
+              className="carousel-item__details--img"
+              src={playIcon}
+              alt="Play Icon"
+            />
+          </Link>
           {
             isList ? 
             <img
---
+++ src/containers/Player.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../assets/styles/components/Player.scss';
+
+const Player = (props) => {
+    const { id } = props;
+
+    return ( 
+        <div className="Player">
+            <video controls={true} autoPlay>
+                <source src="" type="video/mp4"/>
+            </video>
+            <div className="Player-back">
+                <button type="button" onClick={() => {props.history.goBack()}}>
+                    Regresar
+                </button>
+            </div>
+        </div>
+     );
+}
+ 
+export default Player;
